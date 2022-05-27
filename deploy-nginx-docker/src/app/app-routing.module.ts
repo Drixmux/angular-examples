ec3ffc2e68d8fd6d38164790124e40351268ebaa
Fernando Maceda <ferna.mace.34@gmail.com>
2022-05-27 19:11:06 -0400

    deploy angular app using nginx and docker

--- deploy-nginx-docker/src/app/app-routing.module.ts
+++ deploy-nginx-docker/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { OneComponent } from './components/one/one.component';
+import { TwoComponent } from './components/two/two.component';;
+
+const routes: Routes = [
+  { path: 'one', component: OneComponent },
+  { path: 'two', component: TwoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
